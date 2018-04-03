fa0b7047772b0e3546869722915102707b6a310e
arcticsnow <sfihol@alaska.edu>
2018-04-03 14:48:09 +0200

    started script to plot latest 3daysof data for the waspmtoe

---
+++ latice_db/running_plot_finse.py
@@ -0,0 +1,34 @@
+
+'''
+Write script to plot latest week of data for the waspmote data.
+
+'''
+
+import matplotlib.pyplot as plt
+import db_query as db
+import  pandas as pd
+
+
+
+#=============================================
+#   1. import latest 3 days of data into datarame format
+
+
+
+#=============================================
+#   2. plot data
+
+
+
+# wind roses
+
+# thermmistor string
+
+# Temperature, WS, WD
+
+
+
+
+
+
+
